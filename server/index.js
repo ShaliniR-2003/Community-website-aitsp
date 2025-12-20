212f33eecea9363391a17c8fbe4c9d4334061350
ShaliniR-2003 <https://github.com/ShaliniR-2003/git-practice.git>
2025-12-20 13:38:57 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ app.use("/api/posts", require("./routes/posts"));
 app.use("/api/users",require("./routes/users"));
 app.use("/admin", require("./routes/admin"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port",PORT);
 });
